27a84a469aabf4e586bdc1d02f40dd57f79a1061
Thiago Henriques <thiagokxl5@gmail.com>
2021-11-16 10:04:37 -0300

    Corrupcao do codigo

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Thiago","Henriques", "06825786490", "thiagokxl5@gmail.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
